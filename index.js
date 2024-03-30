8aee04fb308179588d94a5550107adc294f8ca51
HafizaKashaf <hafizakashafansari@gmail.com>
2024-03-31 00:00:10 +0500

    added choices

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { massage: 'Enter your first number', type: 'number', name: 'firstnumber' },
-    { massage: 'Enter your second number', type: 'number', name: 'secondnumber' },
-    { massage: 'salect one of the operators to perform operation',
+    { message: 'Enter your first number', type: 'number', name: 'firstnumber' },
+    { message: 'Enter your second number', type: 'number', name: 'secondnumber' },
+    { message: 'salect one of the operators to perform operation',
         type: 'list',
         name: 'operator',
         choices: ["Addition", "subtraction", "Division", "Multiplication"],
